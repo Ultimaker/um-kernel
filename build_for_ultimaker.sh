450ef281701431cd1e537c4037f041b060c1e78e
daid <daid303@gmail.com>
2017-07-28 11:06:57 +0200

    EM-1683, load the device tree depending on the atricle number in the eeprom. Provide different device trees for the UM3v1 and new v2 electronics. Use includes to provide a common base. This should allow for 1 code base for both the v1 and v2 electronics.

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -6,25 +6,25 @@
 # using arm-none-eabi-gcc, so we need to ensure it exists. Because printenv and
 # which can cause bash -e to exit, so run this before setting this up.
 if [ "${CROSS_COMPILE}" == "" ]; then
-	if [ "$(which arm-none-eabi-gcc)" != "" ]; then
-		CROSS_COMPILE="arm-none-eabi-"
-	fi
-	if [ "$(which arm-linux-gnueabihf-gcc)" != "" ]; then
-		CROSS_COMPILE="arm-linux-gnueabihf-"
-	fi
-	if [ "${CROSS_COMPILE}" == "" ]; then
-		echo "No suiteable cross-compiler found."
-		echo "One can be set explicitly via the environment variable CROSS_COMPILE='arm-linux-gnueabihf-' for example."
-		exit 1
-	fi
+    if [ "$(which arm-none-eabi-gcc)" != "" ]; then
+        CROSS_COMPILE="arm-none-eabi-"
+    fi
+    if [ "$(which arm-linux-gnueabihf-gcc)" != "" ]; then
+        CROSS_COMPILE="arm-linux-gnueabihf-"
+    fi
+    if [ "${CROSS_COMPILE}" == "" ]; then
+        echo "No suiteable cross-compiler found."
+        echo "One can be set explicitly via the environment variable CROSS_COMPILE='arm-linux-gnueabihf-' for example."
+        exit 1
+    fi
 fi
 export CROSS_COMPILE=${CROSS_COMPILE}
 
 if [ "${MAKEFLAGS}" == "" ]; then
-	echo -e -n "\e[1m"
-	echo "Makeflags not set, hint, to speed up compilation time, increase the number of jobs. For example:"
-	echo "MAKEFLAGS='-j 4' ${0}"
-	echo -e "\e[0m"
+    echo -e -n "\e[1m"
+    echo "Makeflags not set, hint, to speed up compilation time, increase the number of jobs. For example:"
+    echo "MAKEFLAGS='-j 4' ${0}"
+    echo -e "\e[0m"
 fi
 
 set -e
@@ -36,6 +36,8 @@ KERNEL=`pwd`/linux
 # Which kernel config to build.
 BUILDCONFIG="opinicus"
 
+# Location of the debian package contents
+DEB_DIR=`pwd`/debian
 
 # Setup internal variables
 KCONFIG=`pwd`/configs/${BUILDCONFIG}_config
@@ -45,101 +47,126 @@ KERNEL_BUILD=`pwd`/_build_armhf/${BUILDCONFIG}-linux
 git submodule init
 git submodule update
 
-kernel_build() {
-	RELEASE_VERSION=${RELEASE_VERSION:-9999.99.99}
-
-	# Prepare the build environment
-	mkdir -p ${KERNEL_BUILD}
-	pushd ${KERNEL}
-
-	# Configure the kernel
-	ARCH=arm CROSS_COMPILE="${CROSS_COMPILE}" make O=${KERNEL_BUILD} KCONFIG_CONFIG=${KCONFIG}
-	# Build the uImage file for a bootable kernel
-	ARCH=arm CROSS_COMPILE="${CROSS_COMPILE}" LOADADDR=0x40008000 make O=${KERNEL_BUILD} KCONFIG_CONFIG=${KCONFIG} uImage
-	# Build modules
-	ARCH=arm CROSS_COMPILE="${CROSS_COMPILE}" make O=${KERNEL_BUILD} KCONFIG_CONFIG=${KCONFIG} modules
-	popd
-
-	# Build the debian package
-	DEB_DIR=`pwd`/debian
-
-	# Remove old modules
-	rm -r ${DEB_DIR}/lib 2> /dev/null || true
-	mkdir -p "${DEB_DIR}/boot"
-	cp ${KERNEL_BUILD}/arch/arm/boot/uImage "${DEB_DIR}/boot/uImage-sun7i-a20-opinicus_v1"
-	pushd ${KERNEL}
-	ARCH=arm make CROSS_COMPILE="${CROSS_COMPILE}" O=${KERNEL_BUILD} KCONFIG_CONFIG=${KCONFIG} INSTALL_MOD_PATH="${DEB_DIR}" modules_install
-	popd
-
-	# Build the device trees that we need
-	mkdir -p ${KERNEL_BUILD}/dtb
-	for dts in $(find dts/ -name '*.dts' -exec basename {} \;); do
-		dt=${dts%.dts}
-		echo "Building devicetree blob ${dt}"
-		cpp -nostdinc -undef -D__DTS__ -x assembler-with-cpp \
-			-I${KERNEL}/include -I${KERNEL}/arch/arm/boot/dts \
-			-o ${KERNEL_BUILD}/dtb/.${dt}.dtb.tmp dts/${dts}
-		dtc -I dts -o "${DEB_DIR}/boot/${dt}.dtb" -O dtb ${KERNEL_BUILD}/dtb/.${dt}.dtb.tmp
-	done
-
-	# Generate the boot splash script
-	gcc -Wall -Werror -std=c99 scripts/ultimaker_boot_splash_generator.c -o scripts/ultimaker_boot_splash_generator
-	BOOTSPLASH_COMMANDS=$(scripts/ultimaker_boot_splash_generator)
-
-	# Create the bootscripts for these kernels
-	cat > "${DEB_DIR}/boot/boot_mmc.cmd" <<-EOT
-		setenv bootargs console=tty0 root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4 console=ttyS0,115200 earlyprintk
-		setenv fdt_high 0xffffffff
-		${BOOTSPLASH_COMMANDS}
-		ext4load mmc 0 0x46000000 uImage-sun7i-a20-opinicus_v1
-		ext4load mmc 0 0x49000000 sun7i-a20-opinicus_emmc_v1.dtb
-		bootm 0x46000000 - 0x49000000
-	EOT
-	mkimage -A arm -O linux -T script -C none -a 0x43100000 -n "Boot script" -d "${DEB_DIR}/boot/boot_mmc.cmd" "${DEB_DIR}/boot/boot_mmc.scr"
-
-	cat > "${DEB_DIR}/boot/boot_installer.cmd" <<-EOT
-		setenv bootargs console=tty0 root=/dev/mmcblk0p2 ro rootwait rootfstype=ext4 console=ttyS0,115200 earlyprintk
-		setenv fdt_high 0xffffffff
-		${BOOTSPLASH_COMMANDS}
-		ext4load mmc 0 0x46000000 uImage-sun7i-a20-opinicus_v1
-		ext4load mmc 0 0x49000000 sun7i-a20-opinicus_emmc_v1.dtb
-		bootm 0x46000000 - 0x49000000
-	EOT
-	mkimage -A arm -O linux -T script -C none -a 0x43100000 -n "Boot script" -d "${DEB_DIR}/boot/boot_installer.cmd" "${DEB_DIR}/boot/boot_installer.scr"
-
-	cat > "${DEB_DIR}/boot/boot_emmc.cmd" <<-EOT
-		setenv bootargs console=tty0 root=/dev/mmcblk1p2 ro rootwait rootfstype=f2fs console=ttyS0,115200 earlyprintk
-		setenv fdt_high 0xffffffff
-		${BOOTSPLASH_COMMANDS}
-		ext4load mmc 0 0x46000000 uImage-sun7i-a20-opinicus_v1
-		ext4load mmc 0 0x49000000 sun7i-a20-opinicus_emmc_v1.dtb
-		bootm 0x46000000 - 0x49000000
-	EOT
-	mkimage -A arm -O linux -T script -C none -a 0x43100000 -n "Boot script" -d "${DEB_DIR}/boot/boot_emmc.cmd" "${DEB_DIR}/boot/boot_emmc.scr"
-
-	# Create a debian control file to pack up a debian package
-	mkdir -p "${DEB_DIR}/DEBIAN"
-	cat > "${DEB_DIR}/DEBIAN/control" <<-EOT
-		Package: um-kernel
-		Conflicts: linux-sunxi
-		Replaces: linux-sunxi
-		Version: ${RELEASE_VERSION}
-		Architecture: armhf
-		Maintainer: Anonymous <root@monolith.ultimaker.com>
-		Section: kernel
-		Priority: optional
-		Homepage: http://www.kernel.org/
-		Description: Linux kernel, kernel modules, binary device trees and boot scripts. All in a single package.
-	EOT
-
-	# Build the debian package
-	fakeroot dpkg-deb --build "${DEB_DIR}" um-kernel-${RELEASE_VERSION}.deb
+kernel_build_command() {
+    mkdir -p ${KERNEL_BUILD}
+    pushd ${KERNEL}
+    ARCH=arm CROSS_COMPILE="${CROSS_COMPILE}" make O=${KERNEL_BUILD} KCONFIG_CONFIG=${KCONFIG} $*
+    popd
 }
 
-if [ ${#} -gt 0 ]; then
-	pushd ${KERNEL}
-	ARCH=arm CROSS_COMPILE="${CROSS_COMPILE}" make O=${KERNEL_BUILD} KCONFIG_CONFIG=${KCONFIG} "${@}"
-	popd
-else
-	kernel_build
-fi
+kernel_build() {
+    RELEASE_VERSION=${RELEASE_VERSION:-9999.99.99}
+
+    # Configure the kernel
+    kernel_build_command
+    # Build the uImage file for a bootable kernel
+    kernel_build_command LOADADDR=0x40008000 uImage
+    # Build modules
+    kernel_build_command modules
+}
+
+dts_build() {
+    mkdir -p ${KERNEL_BUILD}/dtb
+    rm -rf ${DEB_DIR}/boot/dtb
+    mkdir -p ${DEB_DIR}/boot/dtb
+    # Build the device trees that we need
+    for dts in $(find dts/ -name '*.dts' -exec basename {} \;); do
+        dt=${dts%.dts}
+        echo "Building devicetree blob ${dt}"
+        cpp -nostdinc -undef -D__DTS__ -x assembler-with-cpp \
+            -I${KERNEL}/include -I${KERNEL}/arch/arm/boot/dts \
+            -o ${KERNEL_BUILD}/dtb/.${dt}.dtb.tmp dts/${dts}
+        dtc -I dts -o "${DEB_DIR}/boot/dtb/${dt}.dtb" -O dtb ${KERNEL_BUILD}/dtb/.${dt}.dtb.tmp
+    done
+
+    while IFS='' read -r LINE || [[ -n "$LINE" ]]; do
+        if [[ $LINE != "#"* && $LINE != "" ]]; then
+            ARTICLE_FULL=$(cut -d':' -f1 <<<"${LINE}")
+            DTS=$(cut -d':' -f2 <<<"${LINE}")
+            
+            ARTICLE_ID=$(cut -d'-' -f1 <<<"${ARTICLE_FULL}")
+            ARTICLE_REV=$(cut -d'-' -s -f2 <<<"${ARTICLE_FULL}")
+            
+            ARTICLE_ID_HEX=$(printf "%x\n" ${ARTICLE_ID})
+            ARTICLE_REV_HEX=$(printf "%x\n" ${ARTICLE_REV})
+            
+            if [[ ${ARTICLE_REV} == "" ]]; then
+                NAME=${ARTICLE_ID_HEX}.dts
+            else
+                NAME=${ARTICLE_ID_HEX}-${ARTICLE_REV_HEX}.dts
+            fi
+            ln -s ${DTS}.dtb ${DEB_DIR}/boot/dtb/${NAME}
+            echo "Created link for article ${ARTICLE_ID} ${ARTICLE_REV}"
+        fi
+    done < "dts/article.links"
+}
+
+script_build() {
+    mkdir -p ${DEB_DIR}/boot
+    
+    # Generate the boot splash script
+    gcc -Wall -Werror -std=c99 scripts/ultimaker_boot_splash_generator.c -o scripts/ultimaker_boot_splash_generator
+    export BOOTSPLASH_COMMANDS=$(scripts/ultimaker_boot_splash_generator)
+
+    # Create the bootscripts for these kernels
+    ROOT_DEV=mmcblk0p2 ROOT_FS=ext4 envsubst '${ROOT_DEV} ${ROOT_FS} ${BOOTSPLASH_COMMANDS}' < scripts/bootscript.cmd > "${DEB_DIR}/boot/boot_mmc.cmd"
+    ROOT_DEV=mmcblk0p2 ROOT_FS=ext4 envsubst '${ROOT_DEV} ${ROOT_FS} ${BOOTSPLASH_COMMANDS}' < scripts/bootscript.cmd > "${DEB_DIR}/boot/boot_installer.cmd"
+    ROOT_DEV=mmcblk1p2 ROOT_FS=f2fs envsubst '${ROOT_DEV} ${ROOT_FS} ${BOOTSPLASH_COMMANDS}' < scripts/bootscript.cmd > "${DEB_DIR}/boot/boot_emmc.cmd"
+
+    # Convert the bootscripts into proper u-boot script images
+    for CMD_FILE in $(find ${DEB_DIR}/boot/ -name '*.cmd' -exec basename {} \;); do
+        SCR_FILE="${CMD_FILE%.*}.scr"
+        mkimage -A arm -O linux -T script -C none -a 0x43100000 -n "Boot script" -d "${DEB_DIR}/boot/${CMD_FILE}" "${DEB_DIR}/boot/${SCR_FILE}"
+    done
+}
+
+deb_build() {
+    # Remove old modules
+    rm -r ${DEB_DIR}/lib 2> /dev/null || true
+    mkdir -p "${DEB_DIR}/boot"
+    # Install kernel image and modules
+    cp ${KERNEL_BUILD}/arch/arm/boot/uImage "${DEB_DIR}/boot/uImage-sun7i-a20-opinicus_v1"
+    kernel_build_command INSTALL_MOD_PATH="${DEB_DIR}" modules_install
+
+    # Create a debian control file to pack up a debian package
+    mkdir -p "${DEB_DIR}/DEBIAN"
+    cat > "${DEB_DIR}/DEBIAN/control" <<-EOT
+        Package: um-kernel
+        Conflicts: linux-sunxi
+        Replaces: linux-sunxi
+        Version: ${RELEASE_VERSION}
+        Architecture: armhf
+        Maintainer: Anonymous <root@monolith.ultimaker.com>
+        Section: kernel
+        Priority: optional
+        Homepage: http://www.kernel.org/
+        Description: Linux kernel, kernel modules, binary device trees and boot scripts. All in a single package.
+EOT
+
+    # Build the debian package
+    fakeroot dpkg-deb --build "${DEB_DIR}" um-kernel-${RELEASE_VERSION}.deb
+}
+
+case ${1-} in
+kernel)
+    kernel_build
+    ;;
+dts)
+    dts_build
+    ;;
+script)
+    script_build
+    ;;
+deb)
+    deb_build
+    ;;
+"")
+    kernel_build
+    dts_build
+    script_build
+    deb_build
+    ;;
+*)
+    kernel_build_command ${1}
+    ;;
+esac
