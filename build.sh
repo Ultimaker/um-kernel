fbc79f13fd708cad22e5627cdbfce6923f7c65d6
Olliver Schinagl <oliver@schinagl.nl>
2018-06-16 23:20:11 +0200

    build: Move submodule initialization out of the build script
    The build script should not worry about preparing the build environment
    that is up for the caller (user, CI etc).
    
    For backwards compatibility with users and build systems, add the repo
    initialization to the old build_for_ultimaker.sh script.
    
    Contributes to issue EMP-141
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- build.sh
+++ build.sh
@@ -68,10 +68,6 @@ fi
 # Set the release version if it's not passed to the script
 RELEASE_VERSION=${RELEASE_VERSION:-9999.99.99}
 
-# Initialize repositories
-git submodule init
-git submodule update
-
 ##
 # busybox_get() - Obtain a statically linked busybox binary
 # param1:	Writable path where to store the retrieved binary
--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -1,1 +1,13 @@
-build.sh
+#!/bin/sh
+#
+# SPDX-License-Identifier: AGPL-3.0+
+#
+# Copyright (C) 2018 Ultimaker B.V.
+# Copyright (C) 2018 Olliver Schinagl <oliver@schinagl.nl>
+#
+
+set -eu
+
+git submodule update --init --recursive
+
+./build.sh "${@}"
