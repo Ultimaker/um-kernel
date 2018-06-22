9ee2bb871739ef1a13c6855b4cd0404255c59fc6
Olliver Schinagl <oliver@schinagl.nl>
2018-06-22 14:46:38 +0200

    build: Add manual build steps to build script using docker
    When invoking the build_for_ultimaker.sh script, it now checks whether
    that docker is available and runs the build job using docker, otherwise
    a native job is started assuming all dependencies are in place.
    
    Fixes issue EMP-218
    
    Signed-off-by: Olliver Schinagl <oliver@schinagl.nl>

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -6,8 +6,50 @@
 # Copyright (C) 2018 Olliver Schinagl <oliver@schinagl.nl>
 #
 
+CI_REGISTRY_IMAGE="${CI_REGISTRY_IMAGE:-registry.gitlab.com/olliver/um-kernel}"
+CI_REGISTRY_IMAGE_TAG="${CI_REGISTRY_IMAGE_TAG:-latest}"
+
+WORKDIR="${WORKDIR:-/build}"
+
 set -eu
+
+FAKE_WHOAMI="$(mktemp --suffix=.sh)"
+cleanup() {
+	rm "${FAKE_WHOAMI}"
+}
+
+fake_whoami() {
+	cat <<- EOT > "${FAKE_WHOAMI}"
+		#!/bin/sh
+
+		if [ "\$(id -u)" -ge 1000 ]; then
+		  echo "$(whoami)"
+		else
+		  whoami "\${@}"
+		fi
+	EOT
+
+	chmod +x "${FAKE_WHOAMI}"
+}
+
+trap cleanup EXIT
 
 git submodule update --init --recursive
 
-./build.sh "${@}"
+if ! command -V docker; then
+	echo "Docker not found, attempting native build."
+
+	./build.sh "${@}"
+	exit 0
+fi
+
+echo "Starting build using ${CI_REGISTRY_IMAGE}:${CI_REGISTRY_IMAGE_TAG}."
+docker pull "${CI_REGISTRY_IMAGE}:${CI_REGISTRY_IMAGE_TAG}"
+fake_whoami
+docker run --rm -i -t -h "$(hostname)" -u "$(id -u)" \
+	   -e "MAKEFLAGS=-j$(($(getconf _NPROCESSORS_ONLN) - 1))" \
+	   -v "$(pwd):${WORKDIR}" \
+	   -v "${FAKE_WHOAMI}:/usr/bin/whoami" \
+	   -w "${WORKDIR}" \
+	   "${CI_REGISTRY_IMAGE}:${CI_REGISTRY_IMAGE_TAG}" \
+	   ./build.sh "${@}"
