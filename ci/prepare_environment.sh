1bcddbb8d9d60b6277688d9fd76317eeac1d3610
martijnbruinenberg <m.bruinenberg@ultimaker.com>
2019-05-31 11:02:03 +0200

    Run correct image

--- ci/prepare_environment.sh
+++ ci/prepare_environment.sh
@@ -2,7 +2,7 @@
 if git whatchanged --name-only --pretty="" master...HEAD | grep "Dockerfile\|.dockerignore"; then
   echo "Dockerfile changes detected..."
   docker build --rm -t "${CI_REGISTRY_IMAGE}:${CI_COMMIT_SHA}" .
-  docker run --rm "$CI_COMMIT_SHA:$CI_PIPELINE_ID" "/test/buildenv_check.sh"
+  docker run --rm "${CI_REGISTRY_IMAGE}:${CI_COMMIT_SHA}" "/test/buildenv_check.sh"
 else
   echo "No Dockerfile changes..."
   docker login -u gitlab-ci-token -p "${CI_JOB_TOKEN}" "${CI_REGISTRY}"
