9fd82770cecf0ce794a998c7b81893f545db6093
Raymond Siudak <r.siudak@ultimaker.com>
2019-07-08 16:55:05 +0200

    Cherry-pick change kernel image based on buildconfig variable
    Contributes to EMP-850
    
    Signed-off-by: Raymond Siudak <r.siudak@ultimaker.com>

--- build.sh
+++ build.sh
@@ -41,6 +41,7 @@ BUILDCONFIG="opinicus"
 # Setup internal variables
 KCONFIG="${CWD}/configs/${BUILDCONFIG}_config"
 KERNEL_BUILD_DIR="${CWD}/_build_armhf/${BUILDCONFIG}-linux"
+KERNEL_IMAGE="uImage-sun7i-a20-opinicus_v1"
 BUILD_OUTPUT_DIR="${CWD}/_build_armhf/"
 DEBIAN_DIR="${BUILD_OUTPUT_DIR}/debian"
 BOOT_FILE_OUTPUT_DIR="${DEBIAN_DIR}/boot"
@@ -247,7 +248,7 @@ kernel_build()
     fi
     mkdir -p "${BOOT_FILE_OUTPUT_DIR}"
 
-    cp "${KERNEL_BUILD_DIR}/arch/arm/boot/uImage" "${BOOT_FILE_OUTPUT_DIR}/uImage-sun7i-a20-opinicus_v1"
+    cp "${KERNEL_BUILD_DIR}/arch/arm/boot/uImage" "${BOOT_FILE_OUTPUT_DIR}/${KERNEL_IMAGE}"
     echo "Finished building Kernel."
 }
 
