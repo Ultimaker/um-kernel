377b7befcb8cf1a3de7a0b8d5754ef8231c8f0c6
Tiemen Joustra <t.joustra@ultimaker.com>
2019-10-04 14:07:40 +0200

    Made convert script run in Python 3

--- initramfs/tools/convertfb.py
+++ initramfs/tools/convertfb.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 #
 # convertfb - program to adapt image to framebuffer
 #
@@ -19,12 +19,13 @@ VERSION=(1,0,0)
 import argparse
 from PIL import Image
 import struct
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-i' , '--input' , dest='img_in'    , metavar='IMAGE' , help="image to handle" , default='demo.bmp')
-parser.add_argument('-o' , '--output', dest='img_out'   , metavar='IMAGE' , help="image to fb"     , default='demo.fb')
+parser.add_argument('-i' , '--input' , dest='img_in'    , metavar='IMAGE' , help="image to handle"  , default='demo.bmp')
+parser.add_argument('-o' , '--output', dest='img_out'   , metavar='IMAGE' , help="image to fb"      , default='demo.fb')
 parser.add_argument('-bw', '--width' , dest='buf_width' , metavar='WIDTH' , help="width of buffer" , type=int)
 parser.add_argument('-bh', '--height', dest='buf_height', metavar='HEIGHT', help="height of buffer", type=int)
-parser.add_argument('-f' , '--format', dest='format'    , metavar='FORMAT', help="format,RGB,BGR,ARGB...",default='RGB')
+parser.add_argument('-f' , '--format', dest='format'    , metavar='FORMAT', help="format,RGB,BGR,ARGB...",default='RGBA')
 
 args = parser.parse_args()
 
@@ -41,31 +42,28 @@ if(args.buf_width==None):
 if(args.buf_height==None):
 	args.buf_height = h
 
-
-
-print 'Image:',args.img_in,' ',w,'X',h,im.mode
-print 'FrameBuffer:',args.buf_width,'X',args.buf_height,args.format
-
+print(f'Image: {args.img_in} {w}X{h,im.mode}')
+print(f'FrameBuffer:{args.buf_width}X{args.buf_height}{args.format}')
 
 #if the size of image larger than than the framebuffer,cut it
 if(w > args.buf_width):
 	w = args.buf_width
-	print 'cut the Image width to',args.buf_width
+	print(f'cut the Image width to {args.buf_width}')
 
 if(h > args.buf_width):
- 	h = args.buf_height
-	print 'cut the Image height to',args.buf_width
+	h = args.buf_height
+	print(f'cut the Image height to {args.buf_width}')
 
 
 with open(args.img_out, 'wb') as f:
-    for j in range(0,h):
-        for i in range(0,w):
-            pixels['R'],pixels['G'],pixels['B'],pixels['A']=im.getpixel((i,j))
-            for n in args.format:
-                f.write(struct.pack('B',pixels[n]))
-        #if the image smaller than the framebuffer,fill in 0
-        for i in range(w,args.buf_width):
-            for n in args.format:
-            	f.write(struct.pack('B',0))
+	for j in range(0,h):
+		for i in range(0,w):
+			pixels['R'],pixels['G'],pixels['B'],pixels['A']=im.getpixel((i,j))
+			for n in args.format:
+				f.write(struct.pack('B',pixels[n]))
+		#if the image smaller than the framebuffer,fill in 0
+		for i in range(w,args.buf_width):
+			for n in args.format:
+				f.write(struct.pack('B',0))
 
 
